8aaafd1e815fb0a3ded9ca470d3c99c60a3265c2
Bogdan <zykovbogdan07@gmail.com>
2022-10-27 13:00:50 +0300

    Initial Commit

---
+++ Qwiker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Qwiker
+//
+//  Created by Богдан Зыков on 27.10.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Qwiker/QwikerApp.swift
@@ -0,0 +1,17 @@
+//
+//  QwikerApp.swift
+//  Qwiker
+//
+//  Created by Богдан Зыков on 27.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct QwikerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
