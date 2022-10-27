e1fafac26eae06529082a5bbc7d5397726a5b900
Bogdan <zykovbogdan07@gmail.com>
2022-10-27 13:35:41 +0300

    init project

---
+++ Qwiker/Utils/Fonts/FontsExt.swift
@@ -0,0 +1,23 @@
+//
+//  FontsExt.swift
+//  Qwiker
+//
+//  Created by Богдан Зыков on 27.10.2022.
+//
+
+
+import SwiftUI
+
+
+extension Font {
+    
+    static func medelRegular(size: Int) -> Font {
+        Font.custom("Medal", size: CGFloat(size))
+    }
+    static func poppinsRegular(size: Int) -> Font {
+        Font.custom("Poppins-Regular", size: CGFloat(size))
+    }
+    static func poppinsMedium(size: Int) -> Font {
+        Font.custom("Poppins-Medium", size: CGFloat(size))
+    }
+}
